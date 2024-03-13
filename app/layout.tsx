93929f0301893c2280d878372e593975461fced1
satya740 <satyaprakash74029@gmail.com>
2024-03-13 12:01:56 +0530

    set-up

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pensieve",
+  description: "Ask anything, instantly. AI-powered PDF Q&A built with Next.js, OpenAI, Stripe, Tailwind & Vercel.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+
+  );
+}
