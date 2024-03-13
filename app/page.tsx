b0d9c2047bf1ecc5d275e426f7535d21aa46cacc
satya740 <satyaprakash74029@gmail.com>
2024-03-13 14:38:38 +0530

    home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,40 @@
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
+
+
+export default async function Home() {
+
+  const { userId} = await auth();
+  const isAuth = !!userId;
   return (
-    <div><Button>Click Me </Button></div>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat With Any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth &&  <Button> Go to Chats</Button> }
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+          Pensieve allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away. 
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (<h1>fileupload</h1>):(
+              <Link href='/sign-in'>
+                <Button>Login to Get Started ! 
+                  <LogIn className="w-4 h-4 ml-2"/>
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
